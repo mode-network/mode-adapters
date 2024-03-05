b162bb52701347ee29209adb213c473409a47182
vokkantthakkar <51082769+vokkantthakkar@users.noreply.github.com>
2024-03-06 01:23:31 +0530

    Renzo adapter

--- projects/example/index.ts
+++ projects/example/index.ts
@@ -1,6 +1,6 @@
 const FACTORY_ADDRESS = '0x9BCE21489a7A85ed475c9e9D0Dc94b4E6A1aEbF3';
 const TOKEN_ADDRESS = '0x2416092f143378750bb29b79eD961ab195CcEea5';
-// documentation for Renzo Manager Contract is 
+// documentation for Renzo Manager Contract is available at https://docs.renzoprotocol.com/docs/contracts/ethereum-mainnet
 const MANAGER_ADDRESS = '0x0x74a09653A083691711cF8215a6ab074BB4e99ef5';
 
 
